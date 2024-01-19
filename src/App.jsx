a63fb29bd6695d1e67174af8b63905990daa794f
Israr Khan <isrark005@gmail.com>
2024-01-19 23:00:17 +0530

    animated cards on about us section

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,29 @@ import { LiaDownloadSolid } from "react-icons/lia";
 import { FaMusic } from "react-icons/fa";
 import  aboutImage  from './assets/about-me.png'
 import  metalImage  from './assets/metal-form.png'
-import heroBg from './assets/hero-bg.png'
+import  nfakImg  from './assets/nfak-img.png'
+import { SlOptions } from "react-icons/sl";
 
 function App() {
   const gradientColor =
     " from-[#DBFF00] via-[#4DF4FF] via--[#DBFF00] to-[#4DF4FF] ";
+    const [overlay, setOverlay] = useState(false)
+
+  const onHover = (e) => {
+    setOverlay(true);
+    
+  }
+  const offHover = (e) => {
+  
+    setOverlay(false);
+  }
+
+
+
 
   return (
     <main>
+    {overlay  && <div className={`overlay w-[100vw] h-[100vh] fixed  bg-black opacity-50 z-[100]`}></div>}
       <div className="first-fold p-6">
         <nav className="nav container-custom flex justify-end gap-5">
           <MenuBtn content={"About us"} />
@@ -69,7 +84,7 @@ function App() {
       </div>
 
       {/* about section */}
-      <section className="about-us container-custom flex">
+      <section className="about-us container-custom flex my-20">
         
         <div className="about-left w-6/12 ">
           <div className="about-wrap p-5 rounded-3xl w-10/12 bg-[#262626] ">
@@ -86,7 +101,7 @@ function App() {
           </div>
         </div>
 
-        <div className="about-right w-6/12">
+        <div className="about-right w-6/12 flex flex-col">
           <h2 className="heading text-6xl">About us</h2>
           <p className="mt-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
@@ -97,7 +112,7 @@ function App() {
             molestiae tempora quidem odio a dignissimos voluptas qui esse
             suscipit.
           </p>
-          <p className="mt-4">
+          <p className="my-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
             ipsa expedita adipisci sequi architecto reiciendis? Eum sed nisi
             aut? Numquam animi molestiae eveniet corrupti cum? Expedita dolorem
@@ -106,6 +121,28 @@ function App() {
             molestiae tempora quidem odio a dignissimos voluptas qui esse
             suscipit.
           </p>
+
+        <div className="cards-container flex-grow-[1] relative">
+           
+            <div onMouseOver={onHover} onMouseLeave={offHover} className="card bg-[#DBFF00] text-black flex  flex-nowrap items-center w-96 rounded-2xl absolute top-0 left-0  hover:translate-y-[-20px] hover:z-[101] transition-all duration-500">
+              <div className="image w-32"><img src={nfakImg} alt="nusrat fateh ali khan singing qawwali"/></div>
+              <div className="title heading text-3xl ml-3">CSS <br />Enthusiast</div>
+              <div className="option-dots ml-auto mr-5 text-3xl text-[#C2E105]"><SlOptions /></div>
+            </div>
+            <div onMouseOver={onHover} onMouseLeave={offHover} className="card bg-[#4CF4FF] text-black flex  flex-nowrap items-center w-96 rounded-2xl absolute top-[90px] left-[55px] rotate-6 hover:rotate-0  hover:translate-y-[-50px] hover:z-[101] transition-all duration-500">
+              <div className="image w-32"><img src={nfakImg} alt="nusrat fateh ali khan singing qawwali"/></div>
+              <div className="title heading text-3xl ml-3">Ghazal Geek <br/> on Fridays</div>
+              <div className="option-dots ml-auto mr-5 text-3xl text-[#2CBFC9]"><SlOptions /></div>
+            </div>
+            <div onMouseOver={onHover} onMouseLeave={offHover} className="card bg-white text-black flex  flex-nowrap items-center w-96 rounded-2xl absolute top-[150px] left-[70px] -rotate-6 hover:rotate-0  hover:translate-y-[-50px] hover:z-[101] transition-all duration-500">
+              <div className="image w-32"><img src={nfakImg} alt="nusrat fateh ali khan singing qawwali"/></div>
+              <div className="title heading text-3xl ml-3">Gaming is a  <br/>love language</div>
+              <div className="option-dots ml-auto mr-5 text-3xl text-[#D9D9D9]"><SlOptions /></div>
+            </div>
+           
+        </div>
+
+
         </div>
       </section>
       {/* end of about section */}
